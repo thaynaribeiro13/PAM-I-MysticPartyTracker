563b91d8da8027315a04959ceca3e085004d0f39
thaynaribeiro13 <ribeirothayna640@gmail.com>
2024-04-22 22:48:34 -0300

    first commit

---
+++ MysticPartyTracker/Model/Dice.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MysticPartyTracker.Model
+{
+    public class Dice
+    {
+        public int Side { get; set; }
+
+        public Dice(int side)
+        {
+            Side = side;
+        }
+
+        public int Roll()
+        {
+            return new Random().Next(1, Side + 1);
+        }
+    }
+}
