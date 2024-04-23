c736610161350942dce8abf1194f817aba5666bf
Bruno Donatelli <bruno.donatelli@etec.sp.gov.br>
2024-04-22 21:53:15 -0300

    feat: add models

---
+++ MysticPartyTracker/Models/Dice.cs
@@ -0,0 +1,12 @@
+﻿namespace MysticPartyTracker.Models
+{
+    public class Dice
+    {
+        public int Side { get; set; }
+
+        public Dice(int side)
+        {
+            Side = side;
+        }
+    }
+}
---
+++ MysticPartyTracker/Views/DiceView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MysticPartyTracker.View;
+
+public partial class DiceView : ContentPage
+{
+    public DiceView()
+    {
+        InitializeComponent();
+    }
+}
